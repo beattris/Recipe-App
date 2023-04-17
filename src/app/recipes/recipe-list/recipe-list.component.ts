5f44d082bdbd5ac88a8953efb016737eb9e64b58
beattris <beatriceebirim@gmail.com>
2023-04-17 22:29:43 +0100

    displayed the recipes details

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -4,10 +4,14 @@ import { Recipe } from '../recipe.model';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent {
   recipes: Recipe[] = [
-    new Recipe('Chicken Breast', 'Chicken breasts are a great way to make your own chicken.', "https://www.onehappyhousewife.com/wp-content/uploads/2019/07/oven-baked-mexican-chicken-breast-9-900x1200.jpg"),
-  ]
+    new Recipe(
+      'Chicken Breast',
+      'Chicken breasts are a great way to make your own chicken.',
+      'https://www.onehappyhousewife.com/wp-content/uploads/2019/07/oven-baked-mexican-chicken-breast-9-900x1200.jpg'
+    ),
+  ];
 }
