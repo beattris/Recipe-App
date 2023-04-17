f680055f253ecd08917f94f76965d9163d8fe04c
beattris <beatriceebirim@gmail.com>
2023-04-17 22:42:43 +0100

    created the ingredient model for the ingredients

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,14 @@
+export class Ingredient {
+    // public name: string;
+    // public amount: number;
+
+    // constructor(name: string, amount: number) {
+    //     this.name = name;
+    //     this.amount = amount;
+    // }
+
+    // THIS CODE BELOW IS A LIKE SHORTCUT TO ACHIEVING EXACTLY THE ABOVE CODE
+    // WHEN THE PROPERTIES ARE JUST FEW
+    
+    constructor(public name: string, public amount: number){}
+}
